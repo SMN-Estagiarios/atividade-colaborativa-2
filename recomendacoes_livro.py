89239871e40025b06d75bf490ecce772d5a405dc
Gustavo Targino <gustavo.targinocc@hotmail.com>
2023-11-13 14:15:32 -0300

    FEAT: Logica filmes recomendados

--- recomendacoes_livro.py
+++ recomendacoes_livro.py
@@ -2,4 +2,12 @@
 
 def obter_recomendacoes(id_usuario):
     # Lógica para gerar recomendações de livros
+
+   id_filmes_recomendados = ['3', '12', '4']
+
+    print(f'Filmes recomendados: {id_filmes_recomendados}')
+
     pass
+
+
+obter_recomendacoes(12)

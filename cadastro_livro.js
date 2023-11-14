9e9d4168024a3853f20225f6c612682711f2a886
OlivioFreitas <ojfneto92@gmail.com>
2023-11-14 08:48:53 -0300

    fix: fixed archive name in readme, called cadastro_geral function

--- cadastro_livro.js
+++ cadastro_livro.js
@@ -1,13 +1,16 @@
 const cadastrar_livro = (titulo, autor, isbn) => {
   const book = {
-    id: titulo + autor,
+    id: isbn + titulo,
     author: autor,
     register: isbn,
   };
+  console.log(book);
 
   return book;
 };
 
 const cadastro_geral = () => {
-  cadastrar_livro();
+  cadastrar_livro("Colheita maldita", "Stephen King", "2587964");
 };
+
+cadastro_geral();
